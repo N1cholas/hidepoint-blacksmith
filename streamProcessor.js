81e7bbc35504398aea228ee746857c71700e89b4
Long <p1ece0fpap3r@gmail.com>
2025-10-14 23:30:14 +0800

    refactor: update data file

--- streamProcessor.js
+++ streamProcessor.js
@@ -3,55 +3,45 @@ import _ from 'lodash'
 
 function extractChinese(text) {
   if (typeof text !== 'string') return ''
-
-  // 匹配Unicode汉字范围（基本汉字+扩展A+兼容+注音符号）
   const chineseRegex = /[\u4e00-\u9fff\u3400-\u4dbf\uf900-\ufaff\u3100-\u312f\u31a0-\u31bf]/g
   const matches = text.match(chineseRegex)
   return matches ? matches.join('') : ''
 }
 
 const errorHandler = (error) => console.error(error)
-const rawFile = 'src\\rawData\\bow.json'
 
-jsonfile
-  .readFile(rawFile)
-  .then((obj) =>
-    obj.normal.map((mod) => ({
-      ...mod,
-      Level: Number(mod.Level),
-      ModGenerationTypeID: Number(mod.ModGenerationTypeID),
-      DropChance: Number(mod.DropChance),
-    })),
-  )
-  .then((normalMods) => {
-    const file = 'src\\data\\bow_normal_mods.json'
-    // {
-    //     Dexterity: [{}, {}, ..., {}],
-    //     LocalAttributeRequirements: [{}, {}, ..., {}],
-    // }
-    const groupedModsObj = _.groupBy(normalMods, 'ModFamilyList')
+async function processMods() {
+  try {
+    const rawFile = 'src/rawData/bow.json'
+    const file = 'src/data/bow_normal_mods.json'
+    const obj = await jsonfile.readFile(rawFile)
 
-    const modsFamilyNames = _.keys(groupedModsObj)
+    // 类型转换+分组一步完成
+    const groupedModsObj = _.groupBy(
+      obj.normal.map((mod) => ({
+        ...mod,
+        Level: Number(mod.Level),
+        ModGenerationTypeID: Number(mod.ModGenerationTypeID),
+        DropChance: Number(mod.DropChance),
+      })),
+      'ModFamilyList',
+    )
 
-    // {
-    //     "normal": [
-    //       {
-    //         "item": [],
-    //         "weight": 8000,
-    //       }
-    //     ]
-    // }
     const fileContent = {
-      // 修改后保持type一致
-      normal: _.values(groupedModsObj).map((mods, i) => ({
+      normal: Object.entries(groupedModsObj).map(([id, mods]) => ({
         items: mods,
         weight: mods.reduce((sum, mod) => sum + mod.DropChance, 0),
-        id: modsFamilyNames[i],
+        id,
         modGenerationTypeID: mods[0].ModGenerationTypeID,
         tags: mods[0].mod_no.map((tag) => extractChinese(tag)),
       })),
     }
 
-    jsonfile.writeFile(file, fileContent, errorHandler)
-  })
-  .catch(errorHandler)
+    await jsonfile.writeFile(file, fileContent)
+    console.log('数据处理完成')
+  } catch (error) {
+    errorHandler(error)
+  }
+}
+
+processMods()
