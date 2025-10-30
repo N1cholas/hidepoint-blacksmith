1e1a979c49c025c6e4d4e90d767011198e6083a8
Long <p1ece0fpap3r@gmail.com>
2025-10-30 23:59:07 +0800

    feat: add some decrypt affix logic

--- src/components/session3/DecryptAffix.vue
+++ src/components/session3/DecryptAffix.vue
@@ -6,9 +6,10 @@ import { computed, ref } from 'vue'
 import { useSession3State } from '@/stores/session3State'
 import { generateAddPool } from '@/utils/generatePool'
 import { useBowNormalModsFamily } from '@/stores/bowNormalMods'
-import { MOD_GENERATION_TYPE, type Affix } from '@/types/types'
+import { MOD_GENERATION_TYPE, type Affix, type AffixFamily } from '@/types/types'
 import { SESSION3_CONFIG } from '@/config/session3Config'
 import { randomlyObtainAffixFamily, randomlyObtainAffix } from '@/utils/randomlyObtain'
+import DecryptList from './DecryptList.vue'
 
 defineProps<{
   name: string
@@ -22,34 +23,16 @@ const disable = computed(() => !session3State.placeholder)
 
 const modGenerationType = session3State.placeholder?.modGenerationTypeID
 
+const newAffixFamilies = ref<AffixFamily[]>([])
+
 // 解密亵渎占位符
 // 展示3条词缀以及重选按钮
-const decryptAffix = () => {
+const generateDecryptPool = (counts: number) => {
   const newAffixFamily = generateAddPool(normalMods.normalModsFamily, itemState.affixFamilies, {
     deduplication: true,
     onlyPrefix: modGenerationType === MOD_GENERATION_TYPE.PREFIX,
     onlySuffix: modGenerationType === MOD_GENERATION_TYPE.SUFFIX,
   })
-
-  const [shouldRemoveAffixFamilyIndex, shouldRemoveAffixIndex] = itemState.findIndexById(
-    SESSION3_CONFIG.PLACEHOLDER_ID,
-  )
-
-  if (newAffixFamily.length) {
-    const hitAffixFamily = randomlyObtainAffixFamily<Affix[]>(newAffixFamily)
-
-    const [minimumLevel, maximumLevel] = session3State.affixLevelRange
-    const hitAffix = randomlyObtainAffix(hitAffixFamily.items, minimumLevel, maximumLevel)
-
-    if (hitAffix) {
-      itemState.replaceAffix(
-        hitAffixFamily,
-        shouldRemoveAffixFamilyIndex,
-        hitAffix,
-        shouldRemoveAffixIndex,
-      )
-    }
-  }
 }
 
 const showModal = ref(false)
@@ -60,10 +43,14 @@ const showModal = ref(false)
   <Teleport to="body">
     <Modal :show="showModal" @close="showModal = false">
       <template #header>
-        <h3>Custom Header</h3>
+        <h3>解密亵渎词缀</h3>
       </template>
       <template #body>
-        <h3>Custom body</h3>
+        <DecryptList
+          :affixes="
+            itemState.affixes.filter((a) => a.ModFamilyList[0] !== SESSION3_CONFIG.PLACEHOLDER_ID)
+          "
+        />
       </template>
     </Modal>
   </Teleport>
